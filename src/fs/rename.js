044613efcc29fac2d74687148cb460a1e6e5ee9e
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-29 12:19:48 +0600

    feat: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
+  const newPath = path.join(__dirname, 'files', 'properFilename.md');
+  return fs.access(newPath, (err) => {
+    if (err) {
+      fs.rename(oldPath, newPath, (err) => {
+        if (err) throw new Error('FS operation failed');
+      });
+    } else {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 await rename();
