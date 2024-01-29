62211fcbf76cda50c9869653fc7a14c59cea9f9d
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-29 12:00:27 +0600

    refactor: add path

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,7 +1,11 @@
 import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
 
 const create = async () => {
-  return fs.writeFile('./src/fs/files/fresh.txt', 'I am fresh and young', { flag: 'wx' }, (err) => {
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const newPath = path.join(__dirname, 'files', 'fresh.txt');
+  return fs.writeFile(newPath, 'I am fresh and young', { flag: 'wx' }, (err) => {
     if (err) throw new Error('FS operation failed');
   });
 };
