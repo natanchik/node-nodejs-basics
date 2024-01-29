b5aa1e079f583ceef0c7f077cf28c4c6da72b051
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-29 23:08:01 +0600

    feat: zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import * as url from 'url';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const readPath = path.join(__dirname, 'files', 'archive.gz');
+  const writePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+  const gzip = createUnzip();
+  const read = createReadStream(readPath);
+  const write = createWriteStream(writePath);
+
+  pipeline(read, gzip, write, (err) => {
+    if (err) console.error(err);
+  });
 };
 
 await decompress();
