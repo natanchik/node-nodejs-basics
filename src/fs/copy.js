92f3ac82582f9f931e828c2ac0e00f0e308b373e
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-29 12:24:40 +0600

    refactor: add return

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -6,7 +6,7 @@ const copy = async () => {
   const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
   const oldPath = path.join(__dirname, 'files');
   const newPath = path.join(__dirname, 'files_copy');
-  fs.cp(oldPath, newPath, { force: false, errorOnExist: true, recursive: true }, (err) => {
+  return fs.cp(oldPath, newPath, { force: false, errorOnExist: true, recursive: true }, (err) => {
     if (err) throw new Error('FS operation failed');
   });
 };
