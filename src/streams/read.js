2f19cbcd20c44be01f28348c95d96e4d4ab2fb2f
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-29 20:17:12 +0600

    feat: streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const stream = fs.createReadStream(filePath);
+  stream.pipe(stdout);
 };
 
 await read();
