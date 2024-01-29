a93bbc057544bbb1742b6cb2065cf2a0d370814f
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-29 18:35:16 +0600

    feat: format env.js answer

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,12 @@
 const parseEnv = () => {
   const args = process.env;
+  const answer = [];
   Object.keys(args).map((arg) => {
     if (arg.startsWith('RSS_')) {
-      console.log(`${arg}=${args[arg]}`);
+      answer.push(`${arg}=${args[arg]}`);
     }
   });
+  console.log(answer.reverse().join('; '));
 };
 
 parseEnv();
