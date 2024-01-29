53c492bca34c2bd35d6bd377e337e8039afeb154
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-29 18:32:17 +0600

    feat: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const args = process.env;
+  Object.keys(args).map((arg) => {
+    if (arg.startsWith('RSS_')) {
+      console.log(`${arg}=${args[arg]}`);
+    }
+  });
 };
 
 parseEnv();
