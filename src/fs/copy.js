d6336b395df6986246015c5e20b58db4a0038656
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-29 12:00:51 +0600

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const oldPath = path.join(__dirname, 'files');
+  const newPath = path.join(__dirname, 'files_copy');
+  fs.cp(oldPath, newPath, { force: false, errorOnExist: true, recursive: true }, (err) => {
+    if (err) throw new Error('FS operation failed');
+  });
 };
 
 await copy();
