13ce6dff5c1bd163a6fbd74983b6bf4535710de7
Natalia Lebedeva <nataliadaveng@gmail.com>
2025-10-26 21:28:34 +0500

    fix: args, update output format

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,8 +1,12 @@
 const parseArgs = () => {
   const args = [...process.argv];
+  const output = [];
+
   for (let i = 2; i < args.length; i += 2) {
-    console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
+    output.push(`${args[i].slice(2)} is ${args[i + 1]}`);
   }
+
+  console.log(output.join(', '));
 };
 
 parseArgs();
